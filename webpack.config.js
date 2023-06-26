cdc774d9699e7e2ff325a91f5aaac4543f58dd6f
murilo9 <murilohenriquematias@gmail.com>
2023-06-25 22:50:29 -0300

    initial structure

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: "ts-loader" }],
+  },
+};
